16c317def2c69ca7b0f21e3fe251a694325b25ce
Jean-Francois James <jefrajames@gmail.com>
2019-10-09 17:10:01 +0200

    Running with KumuluzEE Signed-off-by:Jean-Francois James <jefrajames@gmail.com>

--- bin/04_easy2pay.sh
+++ bin/04_easy2pay.sh
@@ -3,29 +3,26 @@
 clear
 
 EASYPAY_PORT=8080
-PAYARA_MICRO_HOME=~/javatools/payara-micro-5.193
-EASYPAY_WAR_FILE=~/javadev/demo/oco2019/easypay/target/easypay.war
-HSQLDB_HOME=~/javatools/hsqldb-2.5.0
+EASYPAY_JAR_FILE=~/javadev/demo/oco2019/easypay/target/easypay.jar
 
-echo "Starting Easy to Pay with Payara Micro on port $EASYPAY_PORT"
+echo "Starting Easy to Pay with KumuluzEE on port $EASYPAY_PORT"
 
-echo "Hollow Jar deployment, 2 involved files:"
-du -h $EASYPAY_WAR_FILE
-du -h $PAYARA_MICRO_HOME/payara-micro-5.193.jar
+echo "Fat Jar deployment, 1 involved file:"
+du -h $EASYPAY_JAR_FILE
 
 echo ""
 echo ""
 echo ""
 
 
-
-echo "Running Payara Micro with OpenJ9 shared classes"
-CMD="java --add-opens java.base/jdk.internal.loader=ALL-UNNAMED 
--Xshareclasses:name=easypay,bootClassesOnly -Xscmx128m
--jar $PAYARA_MICRO_HOME/payara-micro-5.193.jar
---noCluster --addJars $HSQLDB_HOME/lib/hsqldb.jar --port $EASYPAY_PORT
---deploy $EASYPAY_WAR_FILE"
-
+if [ -z $1 ]
+then
+	echo "Running KumuluzEE with OpenJ9 shared classes"
+	CMD="java -Xshareclasses:name=kumupay -jar $EASYPAY_JAR_FILE"
+else
+	echo "Running KumuluzEE without OpenJ9 shared classes"
+	CMD="java -jar $EASYPAY_JAR_FILE"
+fi
 
 echo $CMD
 
